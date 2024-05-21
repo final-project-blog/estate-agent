d6e8ac3bcdbd819744644e10816d677140d70507
The-ENG-X <eng.ahmadalebrahim@gmail.com>
2024-05-21 10:51:30 +0200

    backend server with mongo connection

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.uri).then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.log(err);
+});
+
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});

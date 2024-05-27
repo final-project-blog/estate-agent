27ef009216fd0c20131fe53fbfc28385ab540a22
Jan <140593990+JanGastmann@users.noreply.github.com>
2024-05-27 13:00:47 +0200

    by_jan

--- api/index.js
+++ api/index.js
@@ -1,34 +1,37 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import authRouter from './routes/auth.route.js';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import authRouter from "./routes/auth.route.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-mongoose.connect(process.env.mongouri).then(() => {
-  console.log('Connected to MongoDB');
-})
-.catch((err) => {
-  console.log(err);
-});
+mongoose
+  .connect(process.env.mongouri)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/user' ,userRouter);s
-app.use('/api/auth', authRouter);
+app.use("/api/user", userRouter);
+s;
+app.use("/api/auth", authRouter);
 
 app.use((err, req, res, next) => {
-    const statusCode = res.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({ 
-        success: false,
-        statusCode,
-        message,
-    });
+  const statusCode = res.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log("Server started on port 3000");
 });

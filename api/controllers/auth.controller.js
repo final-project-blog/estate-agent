9106dff2ce9c59e4e0abeece39562e690d7b9ddc
The-ENG-X <eng.ahmadalebrahim@gmail.com>
2024-05-28 11:28:54 +0200

    sign in function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import * as GoogleStrategy from 'passport-google-oauth20';
+import { errorHandler } from "../utils/errorHandler.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -13,6 +15,24 @@ export const signup = async (req, res, next) => {
     }
     
 };
+
+export const singin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const validUser = await User.findOne({ email });
+      if (!validUser) return next(errorHandler(404, "User not found"));
+      const validUserPassword = bcrypt.compareSync(password, validUser.password);
+      if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+      const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = validUser._doc;
+      res
+        .cookie(accesstoken, token, httpOnly)
+        .status(200)
+        .json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
 
 export const google = async (req, res, next) => {
     passport.use(new GoogleStrategy({

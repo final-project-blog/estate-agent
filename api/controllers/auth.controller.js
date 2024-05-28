0511c371a5d99abbc0a2a86a96ca1998a9216caf
The-ENG-X <eng.ahmadalebrahim@gmail.com>
2024-05-28 11:30:56 +0200

    fix

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -21,7 +21,7 @@ export const singin = async (req, res, next) => {
     try {
       const validUser = await User.findOne({ email });
       if (!validUser) return next(errorHandler(404, "User not found"));
-      const validUserPassword = bcrypt.compareSync(password, validUser.password);
+      const validPassword = bcrypt.compareSync(password, validUser.password);
       if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
       const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = validUser._doc;

25b29c51d49392f7e0ffa926cd6973a338cd04f5
Nour Jazbeh <dr.noorj19@gmail.com>
2024-06-04 08:32:26 +0200

    update Header.jx, Signin.jsx

--- Client/src/pages/Signin.jsx
+++ Client/src/pages/Signin.jsx
@@ -1,9 +1,90 @@
-import React from "react"
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import OAuth from "../components/OAuth";
 
-const Signin = () => {
-    return (
-      <div>Signin</div>
-    )
-  }
-  
-  export default Signin
+const SignIn = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    if (!formData.email || !formData.password) {
+      setError("All fields are required.");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (!data.success) {
+        setError("Invalid username or email.");
+        setLoading(false);
+        return;
+      }
+
+      navigate("/");
+    } catch (error) {
+      setError("An error occurred. Please try again.");
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="p-5 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-10">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="Write your email address"
+          className="border p-3 rounded-lg"
+          id="email"
+          value={formData.email}
+          onChange={handleChange}
+          disabled={loading}
+        />
+        <input
+          type="password"
+          placeholder="Write a strong password"
+          className="border p-3 rounded-lg"
+          id="password"
+          value={formData.password}
+          onChange={handleChange}
+          disabled={loading}
+        />
+        {error && <p className="text-red-500">{error}</p>}
+        <button
+          disabled={loading || !formData.email || !formData.password}
+          className={`bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-80 ${
+            loading ? "opacity-80" : ""
+          }`}
+        >
+          {loading ? "Signing In..." : "Sign In"}
+        </button>
+        <OAuth />
+      </form>
+      <div className="flex gap-2 my-5">
+        <p>Don't have an account?</p>
+        <Link to="/signup" className="text-blue-500">
+          Sign Up
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;

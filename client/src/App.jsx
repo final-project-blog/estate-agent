1fa35521caaa3a6c8e056db3684a634517844fb2
The-ENG-X <eng.ahmadalebrahim@gmail.com>
2024-06-11 11:17:30 +0200

    fix router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Profile from "./pages/Profile"

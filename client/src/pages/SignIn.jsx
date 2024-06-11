1c267d256a2a2e30a5e1b81b04eaaf99dd31fdbc
Nour Jazbeh <dr.noorj19@gmail.com>
2024-06-11 15:59:06 +0200

    added redux , Dockerfile

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,76 +1,88 @@
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
-import OAuth from "../components/OAuth";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
-const SignIn = () => {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
+  const [localError, setLocalError] = useState(null); // Umbenennung von 'error' zu 'localError'
   const [loading, setLoading] = useState(false);
+  const { loading: reduxLoading, error: reduxError } = useSelector((state) => state.user); // Umbenennung von 'error' zu 'reduxError'
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
     try {
-      const res = await fetch("api/auth/signin", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+      setLoading(true);
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-
+      console.log(data);
       if (data.success === false) {
-        setError("invalid Username or Email");
         setLoading(false);
+        setLocalError(data.message); // Verwendung von 'localError' anstelle von 'error'
+        dispatch(signInFailure(data.message));
         return;
       }
       setLoading(false);
-      setError(null);
-      navigate("/");
+      setLocalError(null); // Verwendung von 'localError' anstelle von 'error'
+      dispatch(signInSuccess(data));
+      navigate('/');
     } catch (error) {
-      setError(error.message);
       setLoading(false);
+      setLocalError(error.message); // Verwendung von 'localError' anstelle von 'error'
+      dispatch(signInFailure(error.message));
     }
   };
   return (
-    <div className="p-5 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-10">Sign In</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
-          type="email"
-          placeholder="Write your email address"
-          className="border p-3 rounded-lg "
-          id="email"
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
           onChange={handleChange}
         />
         <input
-          type="password"
-          placeholder="Write a strong password"
-          className="border p-3 rounded-lg "
-          id="password"
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
           onChange={handleChange}
         />
-        {error && <p className="text-red-500">{error}</p>}
         <button
           disabled={loading}
-          className={`bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 ${
-            loading ? "opacity-80" : ""
-          }`}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          Sing In
+          {loading ? 'Loading...' : 'Sign In'}
         </button>
-        <OAuth />
       </form>
-      <div className="flex gap-2 my-5">
+      <div className='flex gap-2 mt-5'>
         <p>Dont have an account?</p>
-        <Link to={"/signup"} className="text-blue-500">
-          Sign Up
+        <Link to={'/sign-up'}>
+          <span className='text-blue-700'>Sign up</span>
         </Link>
       </div>
+      {localError && <p className='text-red-500 mt-5'>{localError}</p>} {/* Verwendung von 'localError' anstelle von 'error' */}
+      {reduxError && <p className='text-red-500 mt-5'>{reduxError}</p>} {/* Verwendung von 'reduxError' anstelle von 'error' */}
     </div>
   );
-};
-
-export default SignIn;
+}

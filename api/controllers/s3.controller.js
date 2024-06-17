27f06507800430cbbbea78280b643b57d7419ebd
The-ENG-X <eng.ahmadalebrahim@gmail.com>
2024-06-17 12:59:28 +0200

    greate download image router

--- api/controllers/s3.controller.js
+++ api/controllers/s3.controller.js
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import AWS from 'aws-sdk';
 import fs from 'fs-extra';
 dotenv.config();
@@ -16,15 +15,7 @@ const s3 = new AWS.S3({
   region
 });
 
-const s3Client = new S3Client({
-  region,
-  credentials: {
-    accessKeyId,
-    secretAccessKey,
-    sessionToken
-  },
-  signatureVersion: 'v4'
-});
+
 const uploadImage = async (req, res, next) => {
     const file = req.file;
     console.log(file);
@@ -40,43 +31,14 @@ const uploadImage = async (req, res, next) => {
       return imageInfo.Location; 
 };
 
+const downloadImage = (fileKey) => {
+  const downloadParams = {
+    Key: fileKey,
+    Bucket: bucketName
+  };
+  
+  return s3.getObject(downloadParams).createReadStream()
+}
 
-export {uploadImage};
 
-// const imageFile = req.file;
-  // console.log(imageFile);
-  // const params = {
-  //   Bucket: bucketName,
-  //   Key: imageFile.originalname,
-  //   Body: imageFile.buffer
-  // };
-
-  // // Upload file to S3
-  // s3.upload(params, (err, data) => {
-  //   if (err) {
-  //     console.error('Error uploading image to S3:', err);
-  //     return res.status(500).send('Error uploading image to S3');
-  //   }
-  //   console.log('Image uploaded to S3:', data.Location);
-  //   res.status(200).send('Image uploaded successfully');
-  // });
-// };
-
-// const rawBytes = await randomBytes(16);
-//   const imageName = rawBytes.toString('hex');
-
-//   const params = {
-//     Bucket: bucketName,
-//     Key: imageName,
-//     Expires: 60
-//   };
-
-//   try {
-//     const command = new PutObjectCommand(params);
-//     const { href: uploadURL } = await s3Client.presign(command);
-//     // res.json({ uploadURL });
-//     console.log(uploadURL);
-//     return uploadURL;
-//   } catch (error) {
-//     console.error('Error generating upload URL:', error);
-//   }
+export {uploadImage, downloadImage};
--- api/routes/s3.route.js
+++ api/routes/s3.route.js
@@ -1,11 +1,16 @@
 import express from 'express';
-import { uploadImage } from '../controllers/s3.controller.js';
+import { downloadImage, uploadImage } from '../controllers/s3.controller.js';
 import multer from 'multer';
 
 
 const routes = express.Router();
 const upload = multer({ dest: 'uploads/' });
-// routes.get('/s3Url', generateUploadURL );
+
 routes.post('/upload',upload.single("image"), uploadImage);
+routes.get('/download/:key', (req, res) => {
+    const key = req.params.key;
+    const readStream = downloadImage(key);
+    return readStream.pipe(res);
+});
 
 export default routes;

2eed0a4b23a997f8613ff2e6aa05aa8c367c658f
Nour Jazbeh <dr.noorj19@gmail.com>
2024-06-17 15:43:47 +0200

    currentUser definieren, fix Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,6 +4,8 @@ import { useSelector } from 'react-redux';
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+
+
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>

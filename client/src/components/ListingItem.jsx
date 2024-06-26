50eb87304ca58d2442d32ceeeaa76f8c0429a21f
The-ENG-X <eng.ahmadalebrahim@gmail.com>
2024-06-26 02:26:29 +0200

    fix bug: delete user listings when account deleted

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -5,8 +5,12 @@ const ListingItem = ({listing}) => {
   return (
     <div className=" bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px] ">
         <Link to={`/listing/${listing._id}`}>
-            <img src={listing.imageUrls[0]} alt="listing cover" 
-            className="w-full h-[320px] sm:h-[220px] object-cover hover:scale-105 transition-scale duration-300 " />
+        {listing.imageUrls && listing.imageUrls.length > 0 ? (
+                <img src={listing.imageUrls[0]} alt="listing cover" 
+                className="w-full h-[320px] sm:h-[220px] object-cover hover:scale-105 transition-scale duration-300 " />
+        ) : (
+                <div className="no-image">No Image Available</div>
+            )}
         <div className=" p-3 flex flex-col gap-2 w-full ">
             <p className=" truncate text-lg font-semibold text-slate-700">{listing.name}</p>
             <div className=" flex items-center gap-1">

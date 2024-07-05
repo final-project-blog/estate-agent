85279a96a2e1137e215ec678963e16ee91b9a26b
unknown <dr.noorj19@gmail.com>
2024-07-05 02:01:04 +0200

    update ingress.yaml file ,estate-agent-application.yaml

--- api/index.js
+++ api/index.js
@@ -19,11 +19,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());

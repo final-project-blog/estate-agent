0f6aa99da810e3ab9790c878b248da6f754d1518
The-ENG-X <eng.ahmadalebrahim@gmail.com>
2024-07-05 09:20:42 +0200

    change cors method

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGO_URI)
 const app = express();
 
 const corsOptions = {
-  origin: '*', 
+  origin: 'http://estate-agent-frontend.s3-website.eu-central-1.amazonaws.com', 
   credentials: true, // allows cookies to be sent
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],

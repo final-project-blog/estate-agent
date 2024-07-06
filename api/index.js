51736980484b4c563323f1db773c364a1f975676
The-ENG-X <eng.ahmadalebrahim@gmail.com>
2024-07-06 21:39:48 +0200

    refactor the backend to work with frontend in the same ec2

--- api/index.js
+++ api/index.js
@@ -18,6 +18,7 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error('MongoDB connection error:', err));
 
 const app = express();
+app.use(express.static('dist'));
 
 const corsOptions = {
   origin: '*', 
